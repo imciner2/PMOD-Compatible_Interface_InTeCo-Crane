263a5864c30bf7ed8c9bcbe7dae8bd595daab065
Ian McInerney <I.McInerney17@imperial.ac.uk>
2019-01-10 19:58:46 +0000

    Cleaned up git

--- 3-Software/ZedboardTest/ZedboardTest.sdk/motorButtonTest/src/helloworld.c
+++ 3-Software/ZedboardTest/ZedboardTest.sdk/motorButtonTest/src/helloworld.c
@@ -48,6 +48,43 @@
 #include <stdio.h>
 #include "platform.h"
 #include "xil_printf.h"
+#include "axi_motor.h"
+
+//Bit 0: BTNC
+//Bit 1: BTND
+//Bit 2: BTNL
+//Bit 3: BTNR
+//Bit 4: BTNU
+typedef struct BUTTONS {
+	uint8_t BTNC:1;
+	uint8_t BTND:1;
+	uint8_t BTNL:1;
+	uint8_t BTNR:1;
+	uint8_t BTNU:1;
+} BUTTONS;
+
+typedef struct SWITCHES {
+	uint8_t SW0:1;
+	uint8_t SW1:1;
+	uint8_t SW2:1;
+	uint8_t SW3:1;
+	uint8_t SW4:1;
+	uint8_t SW5:1;
+	uint8_t SW6:1;
+	uint8_t SW7:1;
+
+}SWITCHES;
+
+typedef struct LEDS {
+	uint8_t LED0:1;
+	uint8_t LED1:1;
+	uint8_t LED2:1;
+	uint8_t LED3:1;
+	uint8_t LED4:1;
+	uint8_t LED5:1;
+	uint8_t LED6:1;
+	uint8_t LED7:1;
+}LEDS;
 
 
 int main()
@@ -55,6 +92,90 @@ int main()
     init_platform();
 
     print("Hello World\n\r");
+
+    BUTTONS* btn = (BUTTONS*) XPAR_AXI_GPIO_0_BASEADDR;
+    LEDS*    led = (LEDS*) XPAR_AXI_GPIO_1_BASEADDR;
+    SWITCHES* sw = (SWITCHES*) XPAR_AXI_GPIO_2_BASEADDR;
+
+    AXI_MOTOR_REG_MAP* mot_x = (AXI_MOTOR_REG_MAP*) XPAR_AXI_MOTOR_0_AXI_BASEADDR;
+    AXI_MOTOR_REG_MAP* mot_y = (AXI_MOTOR_REG_MAP*) XPAR_AXI_MOTOR_1_AXI_BASEADDR;
+    AXI_MOTOR_REG_MAP* mot_z = (AXI_MOTOR_REG_MAP*) XPAR_AXI_MOTOR_2_AXI_BASEADDR;
+
+    mot_x->period = 10000;
+    mot_x->invert_Brake = 0;
+    mot_x->invert_HardLimit = 0;
+    mot_x->invert_therm = 1;
+	mot_x->limitMask_dir0 = 0;
+	mot_x->limitMask_dir1 = 1;
+
+    mot_y->period = 10000;
+    mot_y->invert_Brake = 0;
+    mot_y->invert_HardLimit = 0;
+	mot_y->invert_therm = 0;
+	mot_y->limitMask_dir0 = 0;
+	mot_y->limitMask_dir1 = 1;
+
+    mot_z->period = 10000;
+	mot_z->invert_Brake = 0;
+    mot_z->invert_HardLimit = 0;
+	mot_z->invert_therm = 0;
+	mot_z->limitMask_dir0 = 0;
+	mot_z->limitMask_dir1 = 1;
+
+
+	mot_x->enable = 1;
+	mot_y->enable = 1;
+	mot_z->enable = 1;
+
+    while ( !sw->SW7 ) {
+    	led->LED0 = mot_x->flag_therm;
+    	led->LED1 = mot_x->flag_hardLimit;
+    	led->LED2 = mot_y->flag_therm;
+		led->LED3 = mot_y->flag_hardLimit;
+		led->LED4 = mot_z->flag_therm;
+		led->LED5 = mot_z->flag_hardLimit;
+
+		mot_x->brake = sw->SW0;
+		mot_y->brake = sw->SW1;
+		mot_z->direction = sw->SW2;
+		mot_z->brake = sw->SW3;
+
+		if (btn->BTNC) {
+			mot_z->duty = 2500;
+		} else {
+			mot_z->duty = 0;
+		}
+
+		if (btn->BTND) {
+			mot_y->duty = 2500;
+			mot_y->direction = 1;
+		} else if (btn->BTNU) {
+			mot_y->duty = 2500;
+			mot_y->direction = 0;
+		} else {
+			mot_y->duty = 0;
+			mot_y->direction = 1;
+		}
+
+		if (btn->BTNL) {
+			mot_x->duty = 2500;
+			mot_x->direction = 1;
+		} else if (btn->BTNR) {
+			mot_x->duty = 2500;
+			mot_x->direction = 0;
+		} else {
+			mot_x->duty = 0;
+			mot_x->direction = 1;
+		}
+
+
+    }
+
+	mot_x->enable = 0;
+	mot_y->enable = 0;
+	mot_z->enable = 0;
+
+    print("Goodbye Cruel World\r\n");
 
     cleanup_platform();
     return 0;
